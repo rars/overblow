d474484e07f8db49ab5850e7282cf1f5ea8c5090
Ian Davies <github@iandavies.org>
2012-09-22 23:42:23 +0100

    Added our-bass synth and sliding stuff

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject overblow "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [overtone "0.8.0-SNAPSHOT"]])
+                 [overtone "0.8.0-SNAPSHOT"]]
+  :repl-options {:port 50000})
--- src/overblow/inst/bass.clj
+++ src/overblow/inst/bass.clj
@@ -25,3 +25,43 @@
 (definst pluck-bass
   [freq 440]
   (pluck (* (square freq) (env-gen (perc 0.001 2) :action FREE)) 1 3 (/ 1 freq)))
+
+(defsynth our-bass [f1 50 f2 100 f3 200 amp 3]
+  (let [env     (env-gen (perc 0.05 0.7 1 -5))
+        sig     (* amp env (+ (lpf:ar (saw f1) (* 2 f1))
+                              (lpf:ar (saw f2) (* 2 f2))
+                              (lpf:ar (saw f3) (* 1 f3))))]
+    (out 0 (pan2 sig))))
+
+(defn play-bass [n]
+  (our-bass (midi->hz (- n 12)) (midi->hz n) (midi->hz (+ n 7))))
+
+(def bass-riff
+  [(note :f#3)
+   (note :f#3)
+   [(note :f#3) (note :e3)]
+   [(note :d3) (note :f#3)]
+   nil
+   (note :f#3)
+   [(note :f#3) (note :e3)]
+   [(note :d3) (note :f#3)]
+   (note :e3)
+   (note :e3)
+   [(note :e3) (note :d3)]
+   [(note :c#3) (note :b2)]
+   nil
+   (note :b2)
+   [(note :b2) (note :d3)]
+   [(note :b2) (note :d3)]])
+
+(defn play-bass-riff [metro start-beat [next-note & riff]]
+  (if (coll? next-note)
+    (doseq [[i n] (map-indexed vector next-note)]
+      (when n
+        (at (metro (+ start-beat (* i (/ 1 (count next-note))))) (play-bass n))))
+    (when next-note
+      (at (metro start-beat) (play-bass next-note))))
+  (when riff
+    (apply-at (metro (+ 1 start-beat)) play-bass-riff [metro (+ 1 start-beat) riff])))
+
+(play-bass-riff metro (next-bar-start (count bass-riff)) (apply concat (repeat bass-riff)))
